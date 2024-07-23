315db6fdb819f7516a717b930bc006ccdcf78b80
박유빈 <qkrdbqls1001@naver.com>
2024-07-23 11:07:53 +0900

    chore: 미사용 컨트롤러 삭제 및 CommonController 패키지 위치 정정

--- be/game_server/src/main/java/com/ssafy/a410/common/controller/CommonController.java
+++ be/game_server/src/main/java/com/ssafy/a410/common/controller/CommonController.java
@@ -1,4 +1,4 @@
-package com.ssafy.a410.common.controller.dto;
+package com.ssafy.a410.common.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
