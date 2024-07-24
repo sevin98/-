817d6e98bef7a763ffee90c8fcfb87dc5f9d1002
pakrsevin <psv980817@naver.com>
2024-07-25 00:57:38 +0900

    Feat: 비동기 통신 확인하기 위한 checkPlayerInRoom 메소드 작성

--- be/game_server/src/main/java/com/ssafy/a410/game/controller/RoomController.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/controller/RoomController.java
@@ -1,25 +1,50 @@
 package com.ssafy.a410.game.controller;
 
+import com.ssafy.a410.game.domain.Message;
 import com.ssafy.a410.game.domain.Room;
 import com.ssafy.a410.game.domain.RoomRequest;
 import com.ssafy.a410.game.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
-@RequestMapping("api/rooms")
+@RequestMapping("/api/rooms")
 public class RoomController {
 
     @Autowired
     private RoomService roomService;
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @PostMapping("/create")
     public ResponseEntity<Room> createRoom(@RequestBody RoomRequest roomRequest) {
         Room newRoom = roomService.createRoom(roomRequest);
         return ResponseEntity.ok(newRoom);
+    }
+
+    @GetMapping("/api/rooms/{roomId}/players")
+    public ResponseEntity<Set<String>> getRoomPlayers(@PathVariable String roomId) {
+        Room room = roomService.findRoomById(roomId).orElseThrow(() -> new RuntimeException("Room not found"));
+        Set<String> playerIds = room.getPlayers().keySet();
+        return ResponseEntity.ok(playerIds);
+    }
+
+    @MessageMapping("/check/{roomId}")
+    @SendTo("/topic/check/{roomId}")
+    public String checkPlayerInRoom(@DestinationVariable String roomId, Message message) {
+        Room room = roomService.findRoomById(roomId).orElseThrow(() -> new RuntimeException("Room not found"));
+        if (room.getPlayers().containsKey(message.getSender())) {
+            return "Player " + message.getSender() + " is in room " + roomId;
+        } else {
+            return "Player " + message.getSender() + " is not in room " + roomId;
+        }
     }
 }
