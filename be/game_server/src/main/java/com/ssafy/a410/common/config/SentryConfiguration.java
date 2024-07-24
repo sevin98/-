a9a9227b82fcc6073b69cc44262d46c2e809ebaf
박유빈 <qkrdbqls1001@naver.com>
2024-07-24 09:41:15 +0900

    feat: Sentry 로깅 세팅 + .gradle 추적 제외

---
+++ be/game_server/src/main/java/com/ssafy/a410/common/config/SentryConfiguration.java
@@ -0,0 +1,29 @@
+package com.ssafy.a410.common.config;
+
+import io.sentry.Sentry;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SentryConfiguration {
+    private final String NO_DSN = "NO_DSN";
+
+    @Value("${sentry.cdn:NO_DSN}")
+    private String sentryDsn;
+
+    @PostConstruct
+    public void init() {
+        if (!NO_DSN.equals(sentryDsn)) {
+            Sentry.init(options -> {
+                options.setDsn(sentryDsn);
+            });
+
+            try {
+                throw new Exception("Sentry Test");
+            } catch (Exception e) {
+                Sentry.captureException(e);
+            }
+        }
+    }
+}
