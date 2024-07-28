ff3b18123b078e651572ce48f6ab7032f2f22913
pcjs156 <qkrdbqls1001@gmail.com>
2024-07-28 23:15:08 +0900

    feat: 게임 메시지 송신 서비스 구현 확장

--- be/game_server/src/main/java/com/ssafy/a410/game/service/socket/WebSocketGameBroadcastService.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/service/socket/WebSocketGameBroadcastService.java
@@ -2,16 +2,27 @@ package com.ssafy.a410.game.service.socket;
 
 import com.ssafy.a410.game.domain.Game;
 import com.ssafy.a410.game.domain.Player;
+import com.ssafy.a410.game.domain.Room;
 import com.ssafy.a410.game.domain.Team;
 import com.ssafy.a410.game.service.GameBroadcastService;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+@Slf4j
 @Service
 public class WebSocketGameBroadcastService implements GameBroadcastService {
     private final SimpMessagingTemplate messagingTemplate;
+
+    public WebSocketGameBroadcastService(@Lazy SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @Override
+    public void broadcastTo(Room room, Object message) {
+        messagingTemplate.convertAndSend(room.getTopic(), message);
+    }
 
     @Override
     public void broadcastTo(Game game, Object message) {
