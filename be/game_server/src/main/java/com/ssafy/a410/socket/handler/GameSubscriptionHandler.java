c8428728cb250b43361d68d80057df0e05219c28
pcjs156 <qkrdbqls1001@gmail.com>
2024-07-28 23:29:31 +0900

    feat: `Subscribable` 구현체에 대한 구독 핸들러 작성

---
+++ be/game_server/src/main/java/com/ssafy/a410/socket/handler/GameSubscriptionHandler.java
@@ -0,0 +1,37 @@
+package com.ssafy.a410.socket.handler;
+
+import com.ssafy.a410.common.exception.handler.GameException;
+import com.ssafy.a410.game.domain.Game;
+import com.ssafy.a410.game.domain.Room;
+import com.ssafy.a410.game.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class GameSubscriptionHandler extends SocketSubscriptionHandler {
+    private static final String DESTINATION_PATTERN = "/topic/rooms/[a-zA-Z0-9]+/game";
+
+    private final RoomService roomService;
+
+    @Override
+    public boolean isTarget(String destination) {
+        return destination.matches(DESTINATION_PATTERN);
+    }
+
+    @Override
+    public void handle(String destination, String clientId) {
+    }
+
+    @Override
+    public boolean hasPermission(String destination, String token) {
+        String roomId = getRoomIdFrom(destination);
+        Room room = roomService.findRoomById(roomId).orElseThrow(() -> new GameException("Room not found"));
+        Game game = room.getPlayingGame();
+        return game != null && game.isCorrectSubscriptionToken(token);
+    }
+
+    private String getRoomIdFrom(String destination) {
+        return destination.split("/")[3];
+    }
+}
