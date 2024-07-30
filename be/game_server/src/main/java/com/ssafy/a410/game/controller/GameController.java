ce8fb18c39d04761e9251df29d51921a223dff11
박유빈 <qkrdbqls1001@naver.com>
2024-07-30 16:10:15 +0900

    feat: 게임 시작 시 게임 채널에 플레이어 정보 송신

---
+++ be/game_server/src/main/java/com/ssafy/a410/game/controller/GameController.java
@@ -0,0 +1,23 @@
+package com.ssafy.a410.game.controller;
+
+import com.ssafy.a410.game.service.GameService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class GameController {
+    private final GameService gameService;
+
+    // 요청한 클라이언트에게 개인 채널로 방에 대한 정보를 송신한다.
+    @MessageMapping("/rooms/{roomId}/game")
+    public void sendGameInfoToPlayer(@DestinationVariable String roomId, Principal principal) {
+        gameService.sendGameInfoToPlayer(roomId, principal.getName());
+    }
+}
--- be/game_server/src/main/java/com/ssafy/a410/game/service/memory/MemoryBasedGameService.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/service/memory/MemoryBasedGameService.java
@@ -1,25 +1,40 @@
 package com.ssafy.a410.game.service.memory;
 
 import com.ssafy.a410.game.domain.game.Game;
+import com.ssafy.a410.game.domain.game.message.control.GameInfo;
+import com.ssafy.a410.game.domain.player.Player;
 import com.ssafy.a410.game.service.GameService;
+import com.ssafy.a410.game.service.MessageBroadcastService;
 import com.ssafy.a410.room.domain.Room;
 import com.ssafy.a410.room.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class MemoryBasedGameService implements GameService {
     private final RoomService roomService;
-
-    public MemoryBasedGameService(RoomService roomService) {
-        this.roomService = roomService;
-    }
+    private final MessageBroadcastService messageBroadcastService;
 
     @Override
     public Optional<Game> findGameByPlayerId(String playerId) {
         // roomService에서 방을 찾아와서 playingGame을 가져온다.
         Room room = roomService.getRoomById(playerId);
         return Optional.ofNullable(room.playingGame);
+    }
+
+    @Override
+    public void sendGameInfoToPlayer(String roomId, String userProfileUuid) {
+        // 해당 방에 플레이어가 있는지 먼저 확인하고
+        Room targetRoom = roomService.getRoomById(roomId);
+        Player player = targetRoom.getPlayerWith(userProfileUuid);
+
+        // 게임의 정보를 해당 플레이어의 채널로 전송
+        GameInfo gameInfo = new GameInfo(targetRoom.playingGame);
+        messageBroadcastService.unicastTo(player, gameInfo);
     }
 }
