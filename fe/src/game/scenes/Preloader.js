7a2325e1c5c83abd7d1ec890363ed179ac2ca807
yuan <whyuan@gmail.com>
2024-07-31 03:02:16 +0900

    'feat:지도레이어생성,hp충돌설정'

--- fe/src/game/scenes/Preloader.js
+++ fe/src/game/scenes/Preloader.js
@@ -20,7 +20,7 @@ export class Preloader extends Scene {
         //여기서부터 시작
 
         this.load.tilemapTiledJSON(
-            "map-2024-07-29",
+            "dungeon",
             "/assets/map/map-2024-07-29.json"
         );
         this.load.image("tiles", "/assets/map/map-2024-07-29_tiles.png");
@@ -34,51 +34,21 @@ export class Preloader extends Scene {
             "assets/object/interactionEffect.png"
         );
         this.graphics = this.add.graphics(); //그래픽 객체 생성
-        this.graphics.setDepth(1000); // 항상 제일 위에 그리기
+        // this.graphics.setDepth(10); // 항상 제일 위에 그리기
     }
     create() {
-        
-        const map = this.make.tilemap({
-            key: "map-2024-07-29",
-        });
+        this.cameras.main.setBackgroundColor(0xff0000);
+
+
        // 첫 번째 타일셋 가져오기
-        const tilesetName = map.tilesets[0].name;
-    
         // 타일셋 추가
-        const tiles = map.addTilesetImage(tilesetName, 'tiles');
-    
-        // 레이어 생성
-        const layers = [];
-        for (let i = 0; i < map.layers.length; i++) {
-            layers[i] = map.createLayer(i, tiles, 0, 0);
-            layers[i].setVisible(true);
-        }
-
-         this.cameras.main.setBounds(
-             0,
-             0,
-             map.widthInPixels,
-             map.heightInPixels
-         );
-         this.cameras.main.setScroll(
-             map.widthInPixels / 2,
-             map.heightInPixels / 2
-         );
-          const debugGraphics = this.add.graphics().setAlpha(0.7);
-          map.renderDebug(debugGraphics, {
-              tileColor: null,
-              collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
-              faceColor: new Phaser.Display.Color(40, 39, 37, 255),
-          });
-
-        // const layer1 = map.createLayer("Tile Layer 1", base, 0, 0);
-
+          
 
         // const map0 = this.make.tilemap({ key: "map" });
-        // const tileset = map0.addTilesetImage("map", "tiles");
+        // const tileset = map.addTilesetImage("base", "tiles");
 
-        // map0.createLayer("Ground", tileset);
-        // const wallsLayer = map0.createLayer("Walls", tileset);
+        // map.createLayer("Ground", tileset);
+        // const wallsLayer = map.createLayer("Walls", tileset);
 
         // wallsLayer.setCollisionByProperty({ collides: true });
 
