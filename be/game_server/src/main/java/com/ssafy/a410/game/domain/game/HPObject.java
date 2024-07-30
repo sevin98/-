5aac35f7b9c48673cb0702cb6fa335c3719bb7e1
윤병서 <bsyun0571@naver.com>
2024-07-30 17:58:08 +0900

    feat: 숨어있는 플레이어와 아이템 판별을 위한 요소 추가

--- be/game_server/src/main/java/com/ssafy/a410/game/domain/game/HPObject.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/domain/game/HPObject.java
@@ -1,9 +1,19 @@
 package com.ssafy.a410.game.domain.game;
 
 import com.google.gson.JsonObject;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class HPObject extends GameObject {
+    // 숨어있는 플레이어의 Id, 적용된 아이템 이름
+    private String HidingPlayerId;
+    private String AppliedItemName;
+
     public HPObject(JsonObject jsonObject) {
         super(jsonObject);
+        this.HidingPlayerId = null;
+        this.AppliedItemName = null;
     }
 }
