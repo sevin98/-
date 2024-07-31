7c5b2996b84dfc4b8961c12985d93fab557996f2
윤병서 <bsyun0571@naver.com>
2024-07-31 12:48:56 +0900

    refactor: setter 삭제 및 내부에서만 값 변경하도록 수정

--- be/game_server/src/main/java/com/ssafy/a410/game/domain/game/HPObject.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/domain/game/HPObject.java
@@ -1,19 +1,34 @@
 package com.ssafy.a410.game.domain.game;
 
 import com.google.gson.JsonObject;
+import com.ssafy.a410.game.domain.player.Player;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class HPObject extends GameObject {
-    // 숨어있는 플레이어의 Id, 적용된 아이템 이름
-    private String HidingPlayerId;
-    private String AppliedItemName;
+    // 숨어있거나, 아이템을 숨긴 플레이어
+    private Player player;
+    // 플레이어가 숨어있는 경우 Item = null
+    private Item appliedItem;
 
     public HPObject(JsonObject jsonObject) {
         super(jsonObject);
-        this.HidingPlayerId = null;
-        this.AppliedItemName = null;
+        this.player = null;
+        this.appliedItem = null;
+    }
+
+    // HPObject가 비어있는지 확인하는 함수
+    public boolean isEmpty(){
+        return this.player == null && this.appliedItem == null;
+    }
+
+    // 플레이어를 숨기는 것을 해제하는 함수
+    public void unhidePlayer() {
+        this.player = null;
+    }
+
+    // 아이템을 해제하는 함수
+    public void removeItem() {
+        this.appliedItem = null;
     }
 }
