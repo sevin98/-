217ee957c895ab231da838231580c73387e43693
pakrsevin <psv980817@naver.com>
2024-08-04 23:27:51 +0900

    refactor: 랭킹 누적 반영 대상 변경에 따른 코드 리팩토링
    기존 : AuthInfoEntity
    변경 : UserProfileEntity

--- be/game_server/src/main/java/com/ssafy/a410/auth/model/entity/UserProfileEntity.java
+++ be/game_server/src/main/java/com/ssafy/a410/auth/model/entity/UserProfileEntity.java
@@ -29,4 +29,42 @@ public class UserProfileEntity {
     @Column(name = "role", nullable = false)
     @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    @Column(name = "catch_count", nullable = false)
+    private int catchCount = 0;
+
+    @Column(name = "survival_time_in_seconds", nullable = false)
+    private long survivalTimeInSeconds = 0;
+
+    @Column(name = "wins", nullable = false)
+    private int wins = 0;
+
+    @Column(name = "losses", nullable = false)
+    private int losses = 0;
+
+    public String getFormattedSurvivalTime() {
+        long hours = survivalTimeInSeconds / 3600;
+        long minutes = (survivalTimeInSeconds % 3600) / 60;
+        long seconds = survivalTimeInSeconds % 60;
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    // 경기 종료 후 각 플레이어마다(로그인 한 플레이어라면 catchCount를 누적 시킨다.
+    public void addCatchCount(int count){
+        this.catchCount += count;
+    }
+
+    // 경기 종료 후 각 플레이어마다(로그인 한 플레이어라면 생존시간을 누적 시킨다.
+    public void addSurvivalTimeInSeconds(long seconds){
+        this.survivalTimeInSeconds += seconds;
+    }
+
+    public void addwins(){
+        this.wins += 1;
+    }
+
+    public void addLosses(){
+        this.losses += 1;
+    }
+
 }
