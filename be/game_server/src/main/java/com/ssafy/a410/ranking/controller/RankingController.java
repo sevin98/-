ed516f75ae3f3388cac83ee9064244baadf800d5
pakrsevin <psv980817@naver.com>
2024-08-04 23:27:30 +0900

    refactor: 랭킹 누적 반영 대상 변경에 따른 코드 리팩토링
    기존 : AuthInfoEntity
    변경 : UserProfileEntity

--- be/game_server/src/main/java/com/ssafy/a410/ranking/controller/RankingController.java
+++ be/game_server/src/main/java/com/ssafy/a410/ranking/controller/RankingController.java
@@ -1,6 +1,5 @@
 package com.ssafy.a410.ranking.controller;
 
-import com.ssafy.a410.auth.service.UserService;
 import com.ssafy.a410.ranking.controller.dto.RankingResp;
 import com.ssafy.a410.ranking.service.RankingService;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/rankings")
@@ -22,32 +20,32 @@ public class RankingController {
     public List<RankingResp> getRankingByWins() {
         return rankingService.getAllUsersSortedByWins().stream()
                 .map(user -> new RankingResp(
-                        user.getUserProfile().getNickname(),
+                        user.getNickname(),
                         user.getWins(),
                         user.getCatchCount(),
                         user.getFormattedSurvivalTime()))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @GetMapping("/catch-count")
     public List<RankingResp> getRankingByCatchCount() {
         return rankingService.getAllUsersSortedByCatchCount().stream()
                 .map(user -> new RankingResp(
-                        user.getUserProfile().getNickname(),
+                        user.getNickname(),
                         user.getWins(),
                         user.getCatchCount(),
                         user.getFormattedSurvivalTime()))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @GetMapping("/survival-time")
     public List<RankingResp> getRankingBySurvivalTime() {
         return rankingService.getAllUsersSortedBySurvivalTime().stream()
                 .map(user -> new RankingResp(
-                        user.getUserProfile().getNickname(),
+                        user.getNickname(),
                         user.getWins(),
                         user.getCatchCount(),
                         user.getFormattedSurvivalTime()))
-                .collect(Collectors.toList());
+                .toList();
     }
 }
