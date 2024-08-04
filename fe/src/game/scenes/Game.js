e3335f36ec95bf8bc0d47826ca80dd7dfe2845cb
yuan <whyuan@gmail.com>
2024-08-04 22:48:39 +0900

    fix:conflict해결

--- fe/src/game/scenes/Game.js
+++ fe/src/game/scenes/Game.js
@@ -2,11 +2,7 @@ import Phaser from "phaser";
 
 import { Scene } from "phaser";
 import Player, { Direction, HandlePlayerMove } from "./Player";
-<<<<<<< HEAD
 import OtherPlayer from "./OtherPlayer"
-=======
-import OtherPlayer from "./OtherPlayer";
->>>>>>> a48b53bb9fae14da9acc943424fd8c67e0c74211
 import MapTile from "./MapTile";
 import TextGroup from "./TextGroup";
 import { getRoomRepository } from "../../repository";
@@ -17,7 +13,7 @@ export class game extends Phaser.Scene {
     //fauna = Phaser.Physics.Arcade.Sprite;
     constructor() {
         super("game");
-        /// 임의로 포지션 넣은 코드
+        /// 임의로 포지션 넣은 코드, 이후 삭제
         this.positions = [
             [500, 400],
             [500, 390],
@@ -26,7 +22,6 @@ export class game extends Phaser.Scene {
             [500, 360],
             [500, 350],
         ];
-
 
         this.MapTile = null;
         this.objects = null;
@@ -61,37 +56,9 @@ export class game extends Phaser.Scene {
         const me = this.gameRepository.getMe();
         const { x, y, direction } = me.getPosition();
         this.localPlayer = new Player(this, x, y, "fauna-idle-down", true);
-        this.localPlayer.isRacoon = true;
-<<<<<<< HEAD
+        
         // 잠깐 주석처리하고 카메라가 otherplayer를 따라가도록 바꿔놓음 
         // playercam.startFollow(this.localPlayer);
-=======
-        playercam.startFollow(this.localPlayer);
-
-        const otherPlayerGroup = this.gameRepository.getAllPlayers();
-        otherPlayerGroup.forEach((player) => {
-            if (player.getPlayerId() != me.getPlayerId()) {
-                const { x, y, direction } = player.getPosition(); // this == player
-                this.otherPlayer[player.getPlayerId()] = new OtherPlayer(
-                    this,
-                    x,
-                    y,
-                    "fauna-idle-down",
-                    true,
-                    player.getPlayerId()
-                );
-            }
-            // 숨는팀이고 다른팀일때만 화면에서 안보여야함(같은팀일떄는 보여도됨)
-            if (
-                this.otherPlayer[player.getPlayerId()].IsHidingTeam &&
-                this.otherPlayer[player.getPlayerId()].isRacoon !=
-                    this.localPlayer.isRacoon
-            ) {
-                this.otherPlayer[player.getPlayerId()].visible = false;
-            }
-        });
-        // this.cameras.main.startFollow(this.otherPlayer);
->>>>>>> a48b53bb9fae14da9acc943424fd8c67e0c74211
 
         //로컬플레이어와 layer의 충돌설정
         this.physics.add.collider(
@@ -164,36 +131,6 @@ export class game extends Phaser.Scene {
     }
 
     update() {
-<<<<<<< HEAD
-=======
-        // 클래스의 메서드로 정의
-
-        // 위치 업데이트
-        const otherPlayerGroup = this.gameRepository.getAllPlayers()
-        const me = this.gameRepository.getMe();
-
-        otherPlayerGroup.forEach((player) => {
-            if (player.getPlayerId() != me.getPlayerId()) {
-
-                // 화면에 보이는 다른 player들의 위치와 headDir 변경
-                const { x, y, direction } = player.getPosition();
-                this.otherPlayer[player.getPlayerId()].x = x;
-                this.otherPlayer[player.getPlayerId()].y = y;
-                this.otherPlayer[player.getPlayerId()].move(direction);
-
-                //가시성 업데이트
-                if (
-                    player.IsHidingTeam() &&
-                    player.isRacoonTeam() !== me.isRacoonTeam()
-                ) {
-                    this.otherPlayer[player.getPlayerId()].visible = false;
-                } else {
-                    this.otherPlayer[player.getPlayerId()].visible = true;
-                }
-            }
-        });
->>>>>>> a48b53bb9fae14da9acc943424fd8c67e0c74211
-
         // player.js 에서 player 키조작이벤트 불러옴
         const playerMoveHandler = new HandlePlayerMove(
             this.cursors,
@@ -328,7 +265,6 @@ export class game extends Phaser.Scene {
     tileToPixel(tileCoord) {
         return tileCoord;
     }
-<<<<<<< HEAD
 
     //isHidingTeam (interface.js)에서 가끔 에러나요 이유를 못찾음ㅠ 
     //constructor에 있는 임의의 position 배열에서 좌표 꺼내는 랜덤함수
@@ -342,52 +278,4 @@ export class game extends Phaser.Scene {
         this.otherPlayer.move(this.headDir);
     }
 
-
-
-    // 상우 코드
-    initPhase() {
-        this.readyPhaseEvent = "YET";
-        this.mainPhaseEvent = "YET";
-        this.endPhaseEvent = "YET";
-        this.resultPhaseEvent = "YET";
-    }
-    readyPhaseStart() {
-        this.initPhase();
-        this.readyPhaseEvent = "DURING";
-        if (this.hideTeam === "RACOON" && this.localPlayer.isRacoon) {
-        } else if (this.hideTeam === "FOX" && !this.localPlayer.isRacoon) {
-        } else {
-        }
-    }
-    readyPhaseEnd() {
-        this.readyPhaseEvent = "YET";
-    }
-    mainPhaseStart() {
-        this.initPhase();
-        this.mainPhaseEvent = "DURING";
-        if (this.hideTeam === "RACOON" && this.localPlayer.isRacoon) {
-        } else if (this.hideTeam === "FOX" && !this.localPlayer.isRacoon) {
-        } else {
-        }
-    }
-
-    // sharePlayerPosition() {
-    //   if (webSocketClient.active) {
-    //     webSocketClient.publish({
-    //       destination: `/ws/rooms/${gameStatus.room.id}/players/position`,
-    //       body: JSON.stringify({
-    //           requestId: null,
-    //           data: {
-    //               playerId: gameStatus.player.id,
-    //               x: this.localPlayer.x,
-    //               y: this.localPlayer.y,
-    //               direction: this.getDirectionOfPlayer()
-    //           }
-    //       })
-    //     })
-    //   }
-    // }
 }
-=======
-}
->>>>>>> a48b53bb9fae14da9acc943424fd8c67e0c74211
--- fe/src/game/scenes/OtherPlayer.js
+++ fe/src/game/scenes/OtherPlayer.js
@@ -3,20 +3,13 @@ import { getRoomRepository } from "../../repository";
 import { Phase } from "../../repository/_game";
 
 
-
-
-export default class otherPlayer extends Phaser.Physics.Arcade.Sprite {
+export default class OtherPlayer extends Phaser.Physics.Arcade.Sprite {
     static PLAYER_SPEED = 200;
     static moveX = [0, 1, 0, -1];
     static moveY = [-1, 0, 1, 0];
 
-<<<<<<< HEAD
-    constructor(scene, x, y, texture, id) {
-        super(scene, x, y, texture, id);
-=======
     constructor(scene, x, y, texture,id) {
         super(scene, x, y, texture,id);
->>>>>>> a48b53bb9fae14da9acc943424fd8c67e0c74211
 
         this.id = id;
         this.scale = 1;
@@ -33,14 +26,10 @@ export default class otherPlayer extends Phaser.Physics.Arcade.Sprite {
 
         this.roomRepository = getRoomRepository();
         this.gameRepository = this.roomRepository.getGameRepository();
-<<<<<<< HEAD
 
         // 이후 repo의 정보값으로 대체
         this.isHidingTeam = true;
         this.isRacoon = true;
-=======
-        this.isRacoon = this.gameRepository.getPlayerWithId(id).isRacoonTeam();
->>>>>>> a48b53bb9fae14da9acc943424fd8c67e0c74211
 
         this.setupAnimations();
     }
@@ -242,72 +231,7 @@ export default class otherPlayer extends Phaser.Physics.Arcade.Sprite {
             this.anims.play("fox-idle-down");
         }
     }
-<<<<<<< HEAD
-    //headDir에 따른 움직임 추가
     move(headDir) {
-=======
-    //벽부딫힐때 움직임
-    reflectFromWall(headDir) {
-        this.x -= otherPlayer.moveX[headDir] * otherPlayer.PLAYER_SPEED;
-        this.y -= otherPlayer.moveY[headDir] * otherPlayer.PLAYER_SPEED;
-    }
-
-    stopMove(headDir) {
-        this.setVelocityX(0);
-        this.setVelocityY(0);
->>>>>>> a48b53bb9fae14da9acc943424fd8c67e0c74211
-        if (this.isRacoon) {
-            // 애니메이션 업데이트
-            if (headDir === 0) {
-                // Up
-                this.anims.play("racoon-run-up");
-            } else if (headDir === 1) {
-                // Right
-                this.anims.play("racoon-run-right");
-            } else if (headDir === 2) {
-                // Down
-                this.anims.play("racoon-run-down");
-            } else if (headDir === 3) {
-                // Left
-                this.anims.play("racoon-run-left");
-            }
-        } else {
-            if (headDir === 0) {
-                // Up
-                this.anims.play("fox-run-up");
-            } else if (headDir === 1) {
-                // Right
-                this.anims.play("fox-run-right");
-            } else if (headDir === 2) {
-                // Down
-                this.anims.play("fox-run-down");
-            } else if (headDir === 3) {
-                // Left
-                this.anims.play("fox-run-left");
-            }
-        }
-    }
-<<<<<<< HEAD
-
-    // phase 에 따른 움직임 추가
-    // 내가 하이딩팀일때/ otherplayer 가 하이딩팀일 때 
-    // 언제 otherplayter 화면에 보여지고 안보여지는 
-    // phaser 따라서 구분. 
-
-    // 만약에 나: 하이딩 상대방: 하이딩 -> 상대방보임
-    // 나: 찾 상대방:하이딩 -> this.otherPlayer.visibel == false (if phase==숨는시간)
-}
-
-
-
-=======
-    move(headDir) {
-        this.setVelocityX(
-            otherPlayer.moveX[headDir] * otherPlayer.PLAYER_SPEED
-        );
-        this.setVelocityY(
-            otherPlayer.moveY[headDir] * otherPlayer.PLAYER_SPEED
-        );
         if(this.isRacoon){
             // 애니메이션 업데이트
             if (headDir === 0) {
@@ -340,9 +264,48 @@ export default class otherPlayer extends Phaser.Physics.Arcade.Sprite {
 
         }
 
-
     }
 
+    stopMove(headDir) {
+        if (this.isRacoon) {
+            // 애니메이션 업데이트
+            if (headDir === 0) {
+                // Up
+                this.anims.play("racoon-run-up");
+            } else if (headDir === 1) {
+                // Right
+                this.anims.play("racoon-run-right");
+            } else if (headDir === 2) {
+                // Down
+                this.anims.play("racoon-run-down");
+            } else if (headDir === 3) {
+                // Left
+                this.anims.play("racoon-run-left");
+            }
+        } else {
+            if (headDir === 0) {
+                // Up
+                this.anims.play("fox-run-up");
+            } else if (headDir === 1) {
+                // Right
+                this.anims.play("fox-run-right");
+            } else if (headDir === 2) {
+                // Down
+                this.anims.play("fox-run-down");
+            } else if (headDir === 3) {
+                // Left
+                this.anims.play("fox-run-left");
+            }
+        }
+    }
+    // phase 에 따른 움직임 추가
+    // 내가 하이딩팀일때/ otherplayer 가 하이딩팀일 때 
+    // 언제 otherplayter 화면에 보여지고 안보여지는 
+    // phaser 따라서 구분. 
 
+    // 만약에 나: 하이딩 상대방: 하이딩 -> 상대방보임
+    // 나: 찾 상대방:하이딩 -> this.otherPlayer.visibel == false (if phase==숨는시간)
 }
->>>>>>> a48b53bb9fae14da9acc943424fd8c67e0c74211
+
+
+
