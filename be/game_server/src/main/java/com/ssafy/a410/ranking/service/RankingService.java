bb2b1b6a7d6579431767df6e9f650f4e2ccb3860
pakrsevin <psv980817@naver.com>
2024-08-04 23:27:36 +0900

    refactor: 랭킹 누적 반영 대상 변경에 따른 코드 리팩토링
    기존 : AuthInfoEntity
    변경 : UserProfileEntity

--- be/game_server/src/main/java/com/ssafy/a410/ranking/service/RankingService.java
+++ be/game_server/src/main/java/com/ssafy/a410/ranking/service/RankingService.java
@@ -1,11 +1,11 @@
 package com.ssafy.a410.ranking.service;
 
-import com.ssafy.a410.auth.model.entity.AuthInfoEntity;
+import com.ssafy.a410.auth.model.entity.UserProfileEntity;
 
 import java.util.List;
 
 public interface RankingService {
-    List<AuthInfoEntity> getAllUsersSortedByWins();
-    List<AuthInfoEntity> getAllUsersSortedByCatchCount();
-    List<AuthInfoEntity> getAllUsersSortedBySurvivalTime();
+    List<UserProfileEntity> getAllUsersSortedByWins();
+    List<UserProfileEntity> getAllUsersSortedByCatchCount();
+    List<UserProfileEntity> getAllUsersSortedBySurvivalTime();
 }
