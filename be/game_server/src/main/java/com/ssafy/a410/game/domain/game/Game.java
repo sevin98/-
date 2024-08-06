8dec68ab58a7ac997ec1bd22abffc4512fdf91a3
박세빈 <psv980817@naver.com>
2024-08-06 15:01:43 +0900

    feat: 게임 진행중 안전구역 축소 및 플레이어 처리 / 방송 구현

--- be/game_server/src/main/java/com/ssafy/a410/game/domain/game/Game.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/domain/game/Game.java
@@ -24,10 +24,16 @@ import com.ssafy.a410.socket.domain.Subscribable;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.awt.*;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
+import java.util.Queue;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static com.ssafy.a410.common.exception.ErrorDetail.PLAYER_NOT_IN_ROOM;
@@ -35,6 +41,10 @@ import static com.ssafy.a410.common.exception.ErrorDetail.PLAYER_NOT_IN_ROOM;
 @Getter
 @Slf4j
 public class Game extends Subscribable implements Runnable {
+
+    private static final int SAFE_ZONE_REDUCE_AMOUNT = 100;
+    private static final int SAFE_ZONE_REDUCE_DURATION = 10;
+
     private static final int TOTAL_ROUND = 3;
     // 게임 맵
     private final GameMap gameMap;
@@ -154,6 +164,11 @@ public class Game extends Subscribable implements Runnable {
     public void run() {
         initializeGame();
         for (int round = 1; round <= TOTAL_ROUND && !isGameFinished(); round++) {
+
+            // 첫 라운드 제외하고 계속 맵 줄이기
+            if (round > 1)
+                reduceSafeZoneWithElimination();
+
             // 라운드 변경 알림
             log.debug("Room {} round {} start =======================================", room.getRoomNumber(), round);
             broadcastService.broadcastTo(this, new RoundChangeControlMessage(round, TOTAL_ROUND));
@@ -599,8 +614,8 @@ public class Game extends Subscribable implements Runnable {
     }
 
     // 계산된 hider들의 방향을 seeker에게 전송해주는 메소드
-    public void sendDirectionHints(){
-        for(Player seeker : seekingTeam.getPlayers().values()){
+    public void sendDirectionHints() {
+        for (Player seeker : seekingTeam.getPlayers().values()) {
             List<DirectionArrow> directions = getDirectionsOfHiders(seeker);
             broadcastService.unicastTo(seeker, new DirectionHintMessage(seeker.getId(), directions));
         }
@@ -638,5 +653,33 @@ public class Game extends Subscribable implements Runnable {
         sortedStats.put("FOX", foxTeamStats);
 
         return sortedStats;
+    }
+
+    private void sendSafeZoneUpdate() {
+        List<Point> corners = gameMap.getSafeZoneCorners();
+        broadcastService.broadcastTo(this, new SafeZoneUpdateMessage(corners));
+    }
+
+    private void reduceSafeZoneWithElimination() {
+
+        // 맵 축소
+        gameMap.reduceSafeArea(SAFE_ZONE_REDUCE_AMOUNT);
+        // 안전구역 알림
+        sendSafeZoneUpdate();
+
+        // 10초 후 안전구역 바깥에 있는 플레이어들 탈락처리 메소드 예약
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.schedule(this::eliminatePlayersOutsideSafeZone, SAFE_ZONE_REDUCE_DURATION, TimeUnit.SECONDS);
+    }
+
+    // 안전구역 바깥에 있는 플레이어들 탈락처리
+    private void eliminatePlayersOutsideSafeZone() {
+        List<Player> allPlayers = new ArrayList<>(room.getPlayers().values());
+
+        for (Player player : allPlayers) {
+            if (!gameMap.isInSafeZone(player)) {
+                player.eliminate();
+            }
+        }
     }
 }
