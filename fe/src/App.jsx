95c76395daf671971767dd0334fc40f48f4a692d
윤병서 <bsyun0571@naver.com>
2024-08-06 17:45:17 +0900

    feat: phase.FINISHED 받을 때 이벤트 받도록

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,14 +1,59 @@
+import React, { useState, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "../src/router/router";
-import { ToastContainer } from "react-toastify"; // ToastContainer import
-import "react-toastify/dist/ReactToastify.css"; // ToastContainer 스타일 import
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Popup from "./components/Popup"; // Popup 컴포넌트 import
+// import { LOBBY_ROUTE_PATH } from "../Lobby/Lobby";
 
 function App() {
+    const [isPopupVisible, setPopupVisible] = useState(false);
+
+    useEffect(() => {
+        const handlePhaseFinished = () => {
+            setPopupVisible(true);
+        };
+
+        window.addEventListener("phaseFinished", handlePhaseFinished);
+
+        return () => {
+            window.removeEventListener("phaseFinished", handlePhaseFinished);
+        };
+    }, []);
+
+    const handleExit = async (e) => {
+        try {
+            const res = await axios.post(`/api/rooms/${roomNumber}/leave`, {
+                password: roomPassword,
+            });
+
+            if (res.status === 200) {
+                // navigate(LOBBY_ROUTE_PATH);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        setPopupVisible(false);
+    };
+
+    const handleReturnToLobby = () => {
+        // 방으로 돌아가기 로직 구현
+        console.log("Return to Lobby");
+        // 필요한 추가 방으로 돌아가기 로직
+        setPopupVisible(false);
+    };
+
     return (
         <div id="app">
             <RouterProvider router={router} />
             <ToastContainer />
+            <Popup
+                visible={isPopupVisible}
+                onExit={handleExit}
+                onReturnToLobby={handleReturnToLobby}
+            />
         </div>
     );
 }
+
 export default App;
