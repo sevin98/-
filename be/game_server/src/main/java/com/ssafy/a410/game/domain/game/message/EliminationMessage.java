32af52ba4630debe8db9249cb529d32d4c919e08
박세빈 <psv980817@naver.com>
2024-08-08 17:56:48 +0900

    refactor: ELIMINATION 메시지에 플레이어가 어느 팀인지 추가로 전송

--- be/game_server/src/main/java/com/ssafy/a410/game/domain/game/message/EliminationMessage.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/domain/game/message/EliminationMessage.java
@@ -4,12 +4,16 @@ import com.ssafy.a410.game.domain.game.message.control.GameControlMessage;
 import com.ssafy.a410.game.domain.game.message.control.GameControlType;
 import lombok.Getter;
 
+import java.util.Map;
+
 @Getter
 public class EliminationMessage extends GameControlMessage {
     private final String playerId;
+    private final String team;
 
-    public EliminationMessage(String playerId) {
-        super(GameControlType.ELIMINATION, null);
+    public EliminationMessage(String playerId, String team) {
+        super(GameControlType.ELIMINATION, Map.of("playerId", playerId, "team", team));
         this.playerId = playerId;
+        this.team = team;
     }
 }
