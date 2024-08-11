5665c5caf5689edbb161805a4f6e1d076d1312b3
박세빈 <psv980817@naver.com>
2024-08-12 08:56:53 +0900

    feat: 게임 시작시 전송 message 에 item 추가

--- be/game_server/src/main/java/com/ssafy/a410/game/domain/player/message/control/PlayerInitializeMessage.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/domain/player/message/control/PlayerInitializeMessage.java
@@ -1,13 +1,20 @@
 package com.ssafy.a410.game.domain.player.message.control;
 
+import com.ssafy.a410.game.domain.game.Item;
 import com.ssafy.a410.game.domain.player.PlayerPosition;
 import com.ssafy.a410.game.domain.team.Team;
 import com.ssafy.a410.socket.controller.dto.SubscriptionInfoResp;
 
+import java.util.List;
 import java.util.Map;
 
 public class PlayerInitializeMessage extends PlayerControlMessage {
-    public PlayerInitializeMessage(PlayerPosition playerPosition, Team team) {
-        super(PlayerControlType.INITIALIZE_PLAYER, Map.of("teamCharacter", team.getCharacter(), "playerPositionInfo", playerPosition, "teamSubscriptionInfo", new SubscriptionInfoResp(team)));
+    public PlayerInitializeMessage(PlayerPosition playerPosition, Team team, List<Item> items) {
+        super(PlayerControlType.INITIALIZE_PLAYER, Map.of(
+                "teamCharacter", team.getCharacter(),
+                "playerPositionInfo", playerPosition,
+                "teamSubscriptionInfo", new SubscriptionInfoResp(team),
+                "items", items
+        ));
     }
 }
