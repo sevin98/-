8ffb0c2aec924c496e4b25a5d0043d496fdc815b
pakrsevin <psv980817@naver.com>
2024-08-11 22:02:13 +0900

    feat: 오브젝트 탐색 시 아이템이 있는 오브젝트 처리

--- be/game_server/src/main/java/com/ssafy/a410/game/domain/game/message/request/InteractSeekReq.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/domain/game/message/request/InteractSeekReq.java
@@ -3,7 +3,9 @@ package com.ssafy.a410.game.domain.game.message.request;
 import com.ssafy.a410.game.domain.game.Game;
 import com.ssafy.a410.game.domain.game.GameMap;
 import com.ssafy.a410.game.domain.game.HPObject;
+import com.ssafy.a410.game.domain.game.Item;
 import com.ssafy.a410.game.domain.game.message.control.interact.InteractSeekMessage;
+import com.ssafy.a410.game.domain.game.message.control.item.ItemAppliedMessage;
 import com.ssafy.a410.game.domain.player.Player;
 import com.ssafy.a410.game.domain.team.Team;
 import com.ssafy.a410.game.service.MessageBroadcastService;
@@ -35,7 +37,7 @@ public class InteractSeekReq extends InteractReq {
     public void handle(Player requestedPlayer, Team senderTeam, Game game, MessageBroadcastService broadcastService) {
 
         // requestedPlayer 의 탐색카운트가 허용범위 초과 일 때
-        if(requestedPlayer.getSeekCount() >= MAX_SEEK_COUNT) {
+        if (requestedPlayer.getSeekCount() >= MAX_SEEK_COUNT) {
             InteractSeekMessage message = InteractSeekMessage.failureMessage(
                     roomId,
                     requestedPlayer.getId(),
@@ -76,9 +78,23 @@ public class InteractSeekReq extends InteractReq {
     }
 
     private void handleFailure(HPObject hpObject, Player requestedPlayer, Game game, MessageBroadcastService broadcastService) {
-        hpObject.extractItem();
+        Item installedItem = hpObject.extractItem();
 
-        //TODO: 추후 아이템 적용 로직추가
+        // 해당 오브젝트에 아이템이 있었다면 탐색자에게 적용
+        if (installedItem != null) {
+
+            requestedPlayer.applyItem(installedItem, installedItem.getDuration(), hpObject.getId());
+
+            // 브로드캐스팅
+            broadcastService.broadcastTo(game, new ItemAppliedMessage(
+                    roomId,
+                    requestedPlayer.getId(),
+                    installedItem,
+                    installedItem.getDuration(),
+                    requestedPlayer.getSpeed(),
+                    this.getRequestId()
+            ));
+        }
 
         InteractSeekMessage message = InteractSeekMessage.failureMessage(
                 roomId,
