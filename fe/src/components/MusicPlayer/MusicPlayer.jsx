18b1b50190715c4803333a73558bc2b8b02a9b8e
pcjs156 <qkrdbqls1001@gmail.com>
2024-08-11 23:17:54 +0900

    feat: 페이지 이동 시 배경 음악 변경

--- fe/src/components/MusicPlayer/MusicPlayer.jsx
+++ fe/src/components/MusicPlayer/MusicPlayer.jsx
@@ -1,9 +1,42 @@
 import { useState, useEffect, useRef } from "react";
+
+import { PHASER_GAME_ROUTE_PATH } from "../../game/PhaserGame";
+
 import "./MusicPlayer.css";
 
 export const NORMAL_MUSIC = "/sounds/music/default_music.wav";
 export const FAST_MUSIC = "/sounds/music/fast_music.wav";
-export const PHASER_GAME_ROUTE_PATH = "/phaser-game";
+
+class SoundTrackByRoute {
+    constructor() {
+        this.defaultSoundTrack = NORMAL_MUSIC;
+
+        this.soundTrackByRoute = {
+            [PHASER_GAME_ROUTE_PATH]: FAST_MUSIC,
+        };
+    }
+
+    getSoundTrackByRoute = (route) => {
+        return this.soundTrackByRoute[route] || this.getDefaultSoundTrack();
+    };
+
+    getDefaultSoundTrack = () => {
+        return this.defaultSoundTrack;
+    };
+
+    getRouteBySoundTrack = (soundTrack) => {
+        return Object.keys(this.soundTrackByRoute).find(
+            (route) => this.soundTrackByRoute[route] === soundTrack
+        );
+    };
+
+    isCorrectSoundTrack = (route, soundTrack) => {
+        const expectedSoundTrack = this.getSoundTrackByRoute(route);
+        return soundTrack.endsWith(expectedSoundTrack);
+    };
+}
+
+const soundTrackByRoute = new SoundTrackByRoute();
 
 export default function MusicPlayer() {
     const [bgmTrack, setBgmTrack] = useState(NORMAL_MUSIC);
@@ -23,7 +56,9 @@ export default function MusicPlayer() {
     };
 
     const onMusicEnded = () => {
-        onAudioPlay();
+        if (isPlaying) {
+            onAudioPlay();
+        }
     };
 
     const onSoundButtonClick = () => {
@@ -36,6 +71,36 @@ export default function MusicPlayer() {
     };
 
     // TODO : 게임 화면으로 이동했을 때 배경 음악 바꿔줘야 함
+    useEffect(() => {
+        audioRef.current.volume = 0.5;
+
+        const checkUrlAndUpdateBgm = () => {
+            const currentPath = window.location.pathname;
+            if (
+                soundTrackByRoute.isCorrectSoundTrack(
+                    currentPath,
+                    audioRef.current.src
+                )
+            ) {
+                return;
+            }
+
+            audioRef.current.src =
+                soundTrackByRoute.getSoundTrackByRoute(currentPath);
+            if (isPlaying) {
+                audioRef.current.play();
+            }
+        };
+
+        // Check URL and update BGM track immediately
+        checkUrlAndUpdateBgm();
+
+        // Set up an interval to check the URL periodically
+        const intervalId = setInterval(checkUrlAndUpdateBgm, 1000); // Check every second
+
+        // Clean up the interval on component unmount
+        return () => clearInterval(intervalId);
+    }, [isPlaying]);
 
     return (
         <div className="music-player">
@@ -53,4 +118,3 @@ export default function MusicPlayer() {
         </div>
     );
 }
-
