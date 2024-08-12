6fff902fe18bdfbbaf24359df804be8b7b2b7e31
pakrsevin <psv980817@naver.com>
2024-08-12 23:48:31 +0900

    feat: 게임 결과 전송 위한 GameResultType 추가

--- be/game_server/src/main/java/com/ssafy/a410/game/domain/game/message/control/GameControlType.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/domain/game/message/control/GameControlType.java
@@ -12,4 +12,5 @@ public enum GameControlType {
     GAME_END, // 게임 종료
     ELIMINATION_OUT_OF_SAFE_ZONE, // 안전 구역 밖에서의 탈락
     DIRECTION_HINT, // MUSHROOM 사용 시 , 메인페이즈가 시작 될 때 제공되는 방향 힌트
+    GAME_RESULT, // 게임 결과 메시지
 }
