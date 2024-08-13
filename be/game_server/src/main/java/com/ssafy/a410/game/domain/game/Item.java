f812e8919b2860fa0c3db0a228474d612d9791db
pakrsevin <psv980817@naver.com>
2024-08-13 23:08:13 +0900

    remove: 사용 안하는 주석 제거

--- be/game_server/src/main/java/com/ssafy/a410/game/domain/game/Item.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/domain/game/Item.java
@@ -10,23 +10,17 @@ public enum Item {
     BANANA,
     // 벌통
     BEEHIVE,
-    // 독버섯
-//    POISON_MUSHROOM,
     // 고추
     RED_PEPPER,
     // 표고버섯
     MUSHROOM;
-    // 나뭇잎
-//    LEAF;
 
     public boolean isApplicableToPlayer() {
         return this == MUSHROOM || this == RED_PEPPER;
-//        || this == LEAF;
     }
 
     public boolean isApplicableToHPObject() {
         return this == BANANA || this == BEEHIVE;
-//                || this == POISON_MUSHROOM;
     }
 
     public Duration getDuration() {
@@ -35,14 +29,10 @@ public enum Item {
                 return Duration.ofSeconds(5);
             case RED_PEPPER:
                 return Duration.ofSeconds(10);
-//            case LEAF:
-//                return Duration.ofSeconds(60);
             case BANANA:
                 return Duration.ofSeconds(5);
             case BEEHIVE:
                 return Duration.ofSeconds(5);
-//            case POISON_MUSHROOM:
-//                return Duration.ofSeconds(5);
             default:
                 throw new ResponseException(ErrorDetail.UNKNOWN_ITEM);
         }
