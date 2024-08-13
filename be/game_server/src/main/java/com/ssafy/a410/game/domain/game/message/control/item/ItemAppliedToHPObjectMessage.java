d78aacad46ed0c2b6d876b88a9560d3ad6a32371
박세빈 <psv980817@naver.com>
2024-08-13 16:42:46 +0900

    !Hotfix: data필드에 감싸기

--- be/game_server/src/main/java/com/ssafy/a410/game/domain/game/message/control/item/ItemAppliedToHPObjectMessage.java
+++ be/game_server/src/main/java/com/ssafy/a410/game/domain/game/message/control/item/ItemAppliedToHPObjectMessage.java
@@ -4,6 +4,7 @@ import com.ssafy.a410.game.domain.game.Item;
 import lombok.Getter;
 
 import java.time.Duration;
+import java.util.Map;
 
 @Getter
 public class ItemAppliedToHPObjectMessage extends ItemMessage {
@@ -15,7 +16,16 @@ public class ItemAppliedToHPObjectMessage extends ItemMessage {
     private final String appliedById;
 
     public ItemAppliedToHPObjectMessage(String roomId, String hpObjectId, String playerId, Item item, Duration duration, String appliedById, String requestId) {
-        super(ItemControlType.ITEM_APPLIED_TO_OBJECT, null, requestId);
+        super(ItemControlType.ITEM_APPLIED_TO_OBJECT,
+                Map.of(
+                        "roomId", roomId,
+                        "hpObjectId", hpObjectId,
+                        "playerId", playerId,
+                        "item", item,
+                        "duration", duration,
+                        "appliedById", appliedById
+                ),
+                requestId);
         this.roomId = roomId;
         this.hpObjectId = hpObjectId;
         this.playerId = playerId;
